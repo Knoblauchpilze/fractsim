e559687cebb82cbdfc45fd8e23a35b87e025acc2
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2019-11-26 20:35:07 +0100

    Added first draft of options for mandelbrot set

--- src/MandelbrotOptions.cc
+++ src/MandelbrotOptions.cc
@@ -1,18 +1,81 @@
 
 # include "MandelbrotOptions.hh"
+# include <sdl_graphic/GridLayout.hh>
+# include <sdl_graphic/LabelWidget.hh>
+# include <sdl_graphic/TextBox.hh>
 
 namespace fractsim {
 
   MandelbrotOptions::MandelbrotOptions(const utils::Sizef& hint,
-                                       sdl::core::SdlWidget* parent):
+                                       sdl::core::SdlWidget* parent,
+                                       const sdl::core::engine::Color& labelBgColor):
     sdl::core::SdlWidget(std::string("mandelbrot_options"),
                          hint,
-                         parent)
+                         parent),
+
+    m_labelsBgColor(labelBgColor)
   {
     build();
   }
 
   void
-  MandelbrotOptions::build() {}
+  MandelbrotOptions::build() {
+    // Assign a linear layout which will allow positionning items and icons.
+    sdl::graphic::GridLayoutShPtr layout = std::make_shared<sdl::graphic::GridLayout>(
+      "mandelbrot_options_layout",
+      this,
+      1u,
+      4u
+    );
+
+    // And assign the layout to this widget.
+    setLayout(layout);
+
+    // We need to register the possible options: each one is composed of a
+    // label and of a textbox used to enter the value.
+    sdl::graphic::LabelWidget* powerLabel = new sdl::graphic::LabelWidget(
+      std::string("power_label"),
+      "Power:",
+      getLabelDefaultFontName(),
+      getLabelDefaultFontSize(),
+      sdl::graphic::LabelWidget::HorizontalAlignment::Left,
+      sdl::graphic::LabelWidget::VerticalAlignment::Center,
+      this,
+      m_labelsBgColor
+    );
+
+    sdl::graphic::TextBox* powerValue = new sdl::graphic::TextBox(
+      std::string("power_value"),
+      getValueDefaultFontName(),
+      std::string(),
+      getValueDefaultFontSize(),
+      this
+    );
+
+    sdl::graphic::LabelWidget* accuracyLabel = new sdl::graphic::LabelWidget(
+      std::string("accuracy_label"),
+      "Accuracy:",
+      getLabelDefaultFontName(),
+      getLabelDefaultFontSize(),
+      sdl::graphic::LabelWidget::HorizontalAlignment::Left,
+      sdl::graphic::LabelWidget::VerticalAlignment::Center,
+      this,
+      m_labelsBgColor
+    );
+
+    sdl::graphic::TextBox* accuracyValue = new sdl::graphic::TextBox(
+      std::string("accuracy_value"),
+      getValueDefaultFontName(),
+      std::string(),
+      getValueDefaultFontSize(),
+      this
+    );
+
+    // Add each items to the layout.
+    layout->addItem(powerLabel,    0, 0, 1, 1);
+    layout->addItem(powerValue,    0, 1, 1, 1);
+    layout->addItem(accuracyLabel, 0, 2, 1, 1);
+    layout->addItem(accuracyValue, 0, 3, 1, 1);
+  }
 
 }
--- src/MandelbrotOptions.hxx
+++ src/MandelbrotOptions.hxx
@@ -4,7 +4,31 @@
 # include "MandelbrotOptions.hh"
 
 namespace fractsim {
-  
+
+  inline
+  unsigned
+  MandelbrotOptions::getLabelDefaultFontSize() noexcept {
+    return 15u;
+  }
+
+  inline
+  const char*
+  MandelbrotOptions::getLabelDefaultFontName() noexcept {
+    return "data/fonts/Goodtime.ttf";
+  }
+
+  inline
+  unsigned
+  MandelbrotOptions::getValueDefaultFontSize() noexcept {
+    return 35u;
+  }
+
+  inline
+  const char*
+  MandelbrotOptions::getValueDefaultFontName() noexcept {
+    return "data/fonts/times.ttf";
+  }
+
 }
 
 #endif    /* MANDELBROT_OPTIONS_HXX */
