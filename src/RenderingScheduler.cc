e63ffa51a0f5376f4a8e8ddfe5859d8b7e80383d
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2019-12-07 14:25:25 +0100

    'RenderingScheduler' now starts and exit some threads

--- src/RenderingScheduler.cc
+++ src/RenderingScheduler.cc
@@ -8,11 +8,80 @@ namespace fractsim {
 
     m_statusLocker(),
     m_jobsAvailable(false),
+    m_remaining(0u),
 
     m_jobsLocker(),
-    m_jobs()
+    m_jobs(),
+
+    m_poolLocker(),
+    m_poolRunning(false),
+    m_threadsLocker(),
+    m_threads()
   {
     setService("scheduler");
+    // Create the threads associated to this object.
+    createThreadPool();
+  }
+
+  void
+  RenderingScheduler::createThreadPool() {
+    // Protect from concurrent creation of the pool.
+    Guard guard(m_threadsLocker);
+
+    m_threads.resize(getThreadPoolSize());
+    for (unsigned id = 0u ; id < m_threads.size() ; ++id) {
+      m_threads[id] = std::thread(
+        &RenderingScheduler::jobFetchingLoop,
+        this,
+        id
+      );
+    }
+
+    // Start the pool.
+    Guard guard2(m_poolLocker);
+    m_poolRunning = true;
+  }
+
+  void
+  RenderingScheduler::terminateThreads() {
+    m_poolLocker.lock();
+
+    // If no threads are created, nothing to do.
+    if (!m_poolRunning) {
+      m_poolLocker.unlock();
+      return;
+    }
+
+    // Ask the threads to stop.
+    m_poolRunning = false;
+    m_poolLocker.unlock();
+
+    // Wait for all threads to finish.
+    Guard guard(m_threadsLocker);
+    for (unsigned id = 0u ; id < m_threads.size() ; ++id) {
+      m_threads[id].join();
+    }
+
+    m_threads.clear();
+  }
+
+  void
+  RenderingScheduler::jobFetchingLoop(unsigned threadID) {
+    // Assume we're running.
+    bool running = true;
+
+    log("Creating thread " + std::to_string(threadID) + " for scheduler pool", utils::Level::Notice);
+
+    while (running) {
+      Guard guard(m_poolLocker);
+      running = m_poolRunning;
+
+      if (!running) {
+        break;
+      }
+    }
+
+    log("Terminating thread " + std::to_string(threadID) + " for scheduler pool", utils::Level::Notice);
   }
 
 }
--- src/RenderingScheduler.hh
+++ src/RenderingScheduler.hh
@@ -2,6 +2,7 @@
 # define   RENDERING_SCHEDULER_HH
 
 # include <mutex>
+# include <thread>
 # include <vector>
 # include <memory>
 # include <core_utils/CoreObject.hh>
@@ -18,7 +19,11 @@ namespace fractsim {
        */
       RenderingScheduler();
 
-      ~RenderingScheduler() = default;
+      /**
+       * @brief - Used to destroy the object and terminate all the threads used to
+       *          process the jobs.
+       */
+      ~RenderingScheduler();
 
       /**
        * @brief - Used to notify that some jobs have been inserted into the internal
@@ -51,6 +56,46 @@ namespace fractsim {
     private:
 
       /**
+       * @brief - Provide a default size of the size of the thread pool to create to perform
+       *          the jobs processing. The larger this value the more threads will be created
+       *          in order to be ready for processing jobs.
+       * @return - a value indicating how many threads will be created by this scheduler.
+       */
+      unsigned
+      getThreadPoolSize() noexcept;
+
+      /**
+       * @brief - Used to create the thread pool used by this scheduler to perform the user's
+       *          computations. The threads are created while building this scheduler and are
+       *          waiting for jobs to be enqueued.
+       *          The number of threads to create is retrieved from the static method providing
+       *          a default size for the pool.
+       */
+      void
+      createThreadPool();
+
+      /**
+       * @brief - Used to terminate the threads associated to the thread pool. This is typically
+       *          called upon destroying the scheduler.
+       */
+      void
+      terminateThreads();
+
+      /**
+       * @brief - Used as a thread loop method when creating the pool. This method will be executed
+       *          by each individual thread and handles the querying of the jobs, processing itself,
+       *          notification of the results and termination when needed.
+       *          This method takes an identifier that is used to identify the jobs processed by the
+       *          thread.
+       * @param threadID - a provided counter identifying this thread. Nothing fancy but it allows
+       *                   to easily determine from which thread the completed jobs come from.
+       */
+      void
+      jobFetchingLoop(unsigned threadID);
+
+    private:
+
+      /**
        * @brief - Protect concurrent accesses to the status allowing to notify that
        *          some computing tasks are available.
        */
@@ -64,6 +109,13 @@ namespace fractsim {
       bool m_jobsAvailable;
 
       /**
+       * @brief - This value indicates how many jobs are left to be terminated based
+       *          on the initial count and the number of jobs that already reported
+       *          completion.
+       */
+      unsigned m_remaining;
+
+      /**
        * @brief - Protect concurrent accesses to the jobs queue.
        */
       std::mutex m_jobsLocker;
@@ -72,6 +124,32 @@ namespace fractsim {
        * @brief - The list of jobs currently available for processing.
        */
       std::vector<RenderingTileShPtr> m_jobs;
+
+      /**
+       * @brief - A mutex protecting concurrent accesses to the thread composing the
+       *          pool. Typically used to start or stop the thread pool.
+       */
+      std::mutex m_poolLocker;
+
+      /**
+       * @brief - Keep track of whether the pool is running. As long as this value is
+       *          `true` individual threads can continue fetching information and wait
+       *          for jobs.
+       */
+      bool m_poolRunning;
+
+      /**
+       * @brief - Protect concurrent accesses to the array of threads.
+       */
+      std::mutex m_threadsLocker;
+
+      /**
+       * @brief - The threads used by the pool. When the pool is up and running there
+       *          should be `getThreadPoolSize` threads registered in the vector. A
+       *          termination of the pool destroys the thread but in general they should
+       *          not be accessed directly.
+       */
+      std::vector<std::thread> m_threads;
   };
 
   using RenderingSchedulerShPtr = std::shared_ptr<RenderingScheduler>;
